ea43440673aa4bf41f98e7a55cdef5cd46fbda09
Nurym Kenzh <nak290284@gmail.com>
2019-01-04 09:38:08 +0600

    DB init

---
+++ SmartEcoAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartEcoAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<CompanyKK>()
+            //    .HasIndex(c => c.NameRU);
+
+            //modelBuilder.Entity<Person>()
+            //.HasIndex(p => new { p.FirstName, p.LastName });
+        }
+    }
+}
