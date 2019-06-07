70499417923f71fce5b97270238b42cd9b4f8f19
Nurym Kenzh <nak290284@gmail.com>
2019-06-07 13:14:43 +0600

    Token lifetime -> 1 day

--- SmartEcoAPI/Models/AuthOptions.cs
+++ SmartEcoAPI/Models/AuthOptions.cs
@@ -12,7 +12,7 @@ namespace SmartEcoAPI.Models
         public const string ISSUER = "SmartEco"; // издатель токена
         public const string AUDIENCE = "http://localhost:52207/"; // потребитель токена
         const string KEY = "supersecret_SECretkey!20541";   // ключ для шифрации
-        public const int LIFETIME = 60; // время жизни токена - 60 минут
+        public const int LIFETIME = 60 * 24; // время жизни токена - 60 минут * 24 = сутки
         public static SymmetricSecurityKey GetSymmetricSecurityKey()
         {
             return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(KEY));
