f0da1f6d9166d64823a89fd520e4964846ef0e41
Nurym Kenzh <nak290284@gmail.com>
2019-06-11 12:08:32 +0600

    DB - Karaganda region Kazhydromet monitoring posts

--- SmartEco/Controllers/MonitoringPostsController.cs
+++ SmartEco/Controllers/MonitoringPostsController.cs
@@ -523,6 +523,8 @@ namespace SmartEco.Controllers
             }
             ViewBag.PollutionEnvironments = new SelectList(pollutionEnvironments.OrderBy(m => m.Name), "Id", "Name");
 
+            monitoringPost.AdditionalInformation = monitoringPost.AdditionalInformation.Replace("\r\n", "\r");
+
             return View(monitoringPost);
         }
 
