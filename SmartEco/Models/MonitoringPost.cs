42e9dc5758df72ee1b7f358258ed1da45508568c
Nurym Kenzh <nak290284@gmail.com>
2019-06-11 11:10:24 +0600

    MonitoringPosts - NorthLatitude, EastLongitude

--- SmartEco/Models/MonitoringPost.cs
+++ SmartEco/Models/MonitoringPost.cs
@@ -16,9 +16,11 @@ namespace SmartEco.Models
         [Display(ResourceType = typeof(Resources.Controllers.SharedResources), Name = "Name")]
         public string Name { get; set; }
 
+        [DisplayFormat(DataFormatString = "{0:G}", ApplyFormatInEditMode = true)]
         [Display(ResourceType = typeof(Resources.Controllers.SharedResources), Name = "NorthLatitude")]
         public decimal NorthLatitude { get; set; }
 
+        [DisplayFormat(DataFormatString = "{0:G}", ApplyFormatInEditMode = true)]
         [Display(ResourceType = typeof(Resources.Controllers.SharedResources), Name = "EastLongitude")]
         public decimal EastLongitude { get; set; }
 
