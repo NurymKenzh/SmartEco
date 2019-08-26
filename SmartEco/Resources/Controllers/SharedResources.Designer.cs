c4c5062e8974b087bcd806b976f7935ff048bba9
avtr-elias <ilyaitpr@gmail.com>
2019-08-26 16:39:45 +0300

    Show/hide sidepanels

--- SmartEco/Resources/Controllers/SharedResources.Designer.cs
+++ SmartEco/Resources/Controllers/SharedResources.Designer.cs
@@ -19,7 +19,7 @@ namespace SmartEco.Resources.Controllers {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class SharedResources {
@@ -777,6 +777,24 @@ namespace SmartEco.Resources.Controllers {
         public static string Season {
             get {
                 return ResourceManager.GetString("Season", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to .
+        /// </summary>
+        public static string ShowHideLeftPanelText {
+            get {
+                return ResourceManager.GetString("ShowHideLeftPanelText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to .
+        /// </summary>
+        public static string ShowHideRightPanelText {
+            get {
+                return ResourceManager.GetString("ShowHideRightPanelText", resourceCulture);
             }
         }
         
