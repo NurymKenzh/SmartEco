131e0839f09d2afe6db50eacff9ffcc07fafbf2e
avtr-elias <ilyaitpr@gmail.com>
2020-01-13 13:58:12 +0200

    Kazhydromet: get new data

--- Kazhydromet/Program.cs
+++ Kazhydromet/Program.cs
@@ -16,11 +16,13 @@ namespace Kazhydromet
         {
             public int Id { get; set; }
             public string Name { get; set; }
+            public int KazhydrometID { get; set; }
+            public bool Automatic { get; set; }
         }
         public class MeasuredParameter
         {
             public int Id { get; set; }
-            public string OceanusCode { get; set; }
+            public string KazhydrometCode { get; set; }
             public string NameRU { get; set; }
         }
         public class MeasuredData
@@ -40,18 +42,6 @@ namespace Kazhydromet
             public decimal? MaxValuePerMonth { get; set; }
             public int? PollutionSourceId { get; set; }
         }
-        public class MonitoringPostAutoHand
-        {
-            public int Id { get; set; }
-            public string Name { get; set; }
-            public string Type { get; set; }
-            public int StationId { get; set; }
-        }
-        public class MeasuredParameterCode
-        {
-            public int Id { get; set; }
-            public string Code { get; set; }
-        }
         static void Main(string[] args)
         {
             Console.WriteLine("Program started!");
@@ -62,48 +52,22 @@ namespace Kazhydromet
                 List<MonitoringPost> monitoringPosts = new List<MonitoringPost>();
                 List<MeasuredData> measuredDatas = new List<MeasuredData>();
                 List<MeasuredParameter> measuredParameters = new List<MeasuredParameter>();
-                List<MonitoringPostAutoHand> monitoringPostsAutoHand = new List<MonitoringPostAutoHand>
-                {
-                    new MonitoringPostAutoHand { Id = 15, Name = "ПНЗ №2", Type = "A", StationId = 25},
-                    new MonitoringPostAutoHand { Id = 11, Name = "ПНЗ №29", Type = "A", StationId = 26},
-                    new MonitoringPostAutoHand { Id = 13, Name = "ПНЗ №31", Type = "A", StationId = 27},
-                    new MonitoringPostAutoHand { Id = 12, Name = "ПНЗ №30", Type = "A", StationId = 28},
-                    new MonitoringPostAutoHand { Id = 9, Name = "ПНЗ №27", Type = "A", StationId = 55},
-                    new MonitoringPostAutoHand { Id = 14, Name = "ПНЗ №1", Type = "A", StationId = 56},
-                    new MonitoringPostAutoHand { Id = 17, Name = "ПНЗ №4", Type = "A", StationId = 57},
-                    new MonitoringPostAutoHand { Id = 16, Name = "ПНЗ №3", Type = "A", StationId = 58},
-                    new MonitoringPostAutoHand { Id = 19, Name = "ПНЗ №6", Type = "A", StationId = 59},
-                    new MonitoringPostAutoHand { Id = 18, Name = "ПНЗ №5", Type = "A", StationId = 60},
-                    new MonitoringPostAutoHand { Id = 10, Name = "ПНЗ №28", Type = "A", StationId = 97},
-                    new MonitoringPostAutoHand { Id = 4, Name = "ПНЗ №1", Type = "R", StationId = 102},
-                    new MonitoringPostAutoHand { Id = 5, Name = "ПНЗ №12", Type = "R", StationId = 103},
-                    new MonitoringPostAutoHand { Id = 6, Name = "ПНЗ №16", Type = "R", StationId = 104},
-                    new MonitoringPostAutoHand { Id = 7, Name = "ПНЗ №25", Type = "R", StationId = 105},
-                    new MonitoringPostAutoHand { Id = 8, Name = "ПНЗ №26", Type = "R", StationId = 106}
-                };
-                List<MeasuredParameterCode> measuredParametersCode = new List<MeasuredParameterCode>
-                {
-                    new MeasuredParameterCode { Id = 2, Code = "PM10"},
-                    new MeasuredParameterCode { Id = 3, Code = "PM2.5"},
-                    new MeasuredParameterCode { Id = 7, Code = "CO"},
-                    new MeasuredParameterCode { Id = 13, Code = "NO2"}
-                };
                 try
                 {
-                    //using (var connection = new NpgsqlConnection("Host=localhost;Database=SmartEcoAPI;Username=postgres;Password=postgres"))
-                    //{
-                    //    connection.Open();
+                    using (var connection = new NpgsqlConnection("Host=localhost;Database=SmartEcoAPI;Username=postgres;Password=postgres"))
+                    {
+                        connection.Open();
 
-                    //    var monitoringPostsv = connection.Query<MonitoringPost>(
-                    //        $"SELECT \"Id\", \"Name\"" +
-                    //        $"FROM public.\"MonitoringPost\" WHERE \"DataProviderId\" = 1 and \"ProjectId\" = 3");
-                    //    monitoringPosts = monitoringPostsv.ToList();
+                        var monitoringPostsv = connection.Query<MonitoringPost>(
+                            $"SELECT \"Id\", \"Name\", \"KazhydrometID\", \"Automatic\"" +
+                            $"FROM public.\"MonitoringPost\" WHERE \"DataProviderId\" = 1 and \"ProjectId\" = 3");
+                        monitoringPosts = monitoringPostsv.ToList();
 
-                    //    var measuredParametersv = connection.Query<MeasuredParameter>(
-                    //    $"SELECT \"Id\", \"NameRU\", \"OceanusCode\"" +
-                    //    $"FROM public.\"MeasuredParameter\" WHERE \"OceanusCode\" <> '' and \"OceanusCode\" is not null;");
-                    //    measuredParameters = measuredParametersv.ToList();
-                    //}
+                        var measuredParametersv = connection.Query<MeasuredParameter>(
+                        $"SELECT \"Id\", \"NameRU\", \"KazhydrometCode\"" +
+                        $"FROM public.\"MeasuredParameter\" WHERE \"KazhydrometCode\" <> '' and \"KazhydrometCode\" is not null;");
+                        measuredParameters = measuredParametersv.ToList();
+                    }
 
                     //Get Data Automatic Posts
                     if ((DateTime.Now - lastInsertAutoPostsTime) > new TimeSpan(0, 1, 0, 0))
@@ -120,24 +84,26 @@ namespace Kazhydromet
                             jsonString = reader.ReadToEnd();
                         }
                         dynamic datas = JArray.Parse(jsonString);
-                        var monitoringPostsAuto = monitoringPostsAutoHand.Where(m => m.Type == "A");
+                        //var monitoringPostsAuto = monitoringPostsAutoHand.Where(m => m.Type == "A");
 
                         foreach (dynamic data in datas)
                         {
                             int stationId = data.stationId;
                             string code = data.code;
-                            var monitoringPostAuto = monitoringPostsAuto.Where(m => m.StationId == stationId).ToList();
-                            if (monitoringPostAuto.Count != 0)
+                            var monitoringPost = monitoringPosts.Where(m => m.KazhydrometID == stationId).ToList();
+                            if (monitoringPost.Count != 0)
                             {
-                                var measuredParameterCode = measuredParametersCode.Where(m => m.Code == code).ToList();
-                                if (measuredParameterCode.Count != 0)
+                                var measuredParameter = measuredParameters.Where(m => m.KazhydrometCode == code).ToList();
+                                if (measuredParameter.Count != 0)
                                 {
                                     var value = data.value != null ? Convert.ToDecimal(data.value) : null;
+                                    DateTime date = Convert.ToDateTime(data.date);
+                                    date = date.AddHours(6);   //Greenwich conversion
                                     measuredDatas.Add(new MeasuredData()
                                     {
-                                        DateTime = data.date,
-                                        MeasuredParameterId = measuredParameterCode[0].Id,
-                                        MonitoringPostId = monitoringPostAuto[0].Id,
+                                        DateTime = date,
+                                        MeasuredParameterId = measuredParameter[0].Id,
+                                        MonitoringPostId = monitoringPost[0].Id,
                                         Value = value,
                                         Averaged = true
                                     });
@@ -186,11 +152,11 @@ namespace Kazhydromet
                         Console.WriteLine($"{DateTime.Now.ToString()} >> Get Data from Kazhydromet Hands Posts started.{Environment.NewLine}");
 
                         measuredDatas.Clear();
-                        var monitoringPostHands = monitoringPostsAutoHand.Where(m => m.Type == "R").ToList();
+                        var monitoringPostHands = monitoringPosts.Where(m => m.Automatic == false).ToList();
                         string dateTime = DateTime.Now.ToString("yyyy-MM-dd");
-                        foreach (var monitoringPostHand in monitoringPostHands)
+                        foreach (var monitoringPost in monitoringPostHands)
                         {
-                            HttpWebRequest WebReqHand = (HttpWebRequest)WebRequest.Create(string.Format($"http://93.185.75.19:4003/simple/averages?stationNumber={monitoringPostHand.StationId}&after={dateTime}&key=be927b4b6fbd304984b72d1456c098860b57ee0692f3d9634a41a230272f73d0"));
+                            HttpWebRequest WebReqHand = (HttpWebRequest)WebRequest.Create(string.Format($"http://93.185.75.19:4003/simple/averages?stationNumber={monitoringPost.KazhydrometID}&after={dateTime}&key=be927b4b6fbd304984b72d1456c098860b57ee0692f3d9634a41a230272f73d0"));
                             WebReqHand.Method = "GET";
                             string jsonStringHand;
                             HttpWebResponse WebRespHand = (HttpWebResponse)WebReqHand.GetResponse();
@@ -204,16 +170,18 @@ namespace Kazhydromet
                             foreach (dynamic data in datasHand)
                             {
                                 string code = data.code;
-                                var measuredParameterCode = measuredParametersCode.Where(m => m.Code == code).ToList();
-                                if (measuredParameterCode.Count != 0)
+                                var measuredParameter = measuredParameters.Where(m => m.KazhydrometCode == code).ToList();
+                                if (measuredParameter.Count != 0)
                                 {
-                                    var value = data.value != null ? data.value : null;
+                                    var value = data.value != null ? Convert.ToDecimal(data.value) : null;
+                                    DateTime date = Convert.ToDateTime(data.date);
+                                    date = date.AddHours(6);   //Greenwich conversion
                                     measuredDatas.Add(new MeasuredData()
                                     {
-                                        DateTime = data.date,
-                                        MeasuredParameterId = measuredParameterCode[0].Id,
-                                        MonitoringPostId = monitoringPostHand.Id,
-                                        Value = Convert.ToDecimal(value),
+                                        DateTime = date,
+                                        MeasuredParameterId = measuredParameter[0].Id,
+                                        MonitoringPostId = monitoringPost.Id,
+                                        Value = value,
                                         Averaged = true
                                     });
                                 }
