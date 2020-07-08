c6d9d4347b5df075964160952386306115d040e0
avtr-elias <ilyaitpr@gmail.com>
2020-07-08 13:33:40 +0300

    Air Pollutant Model

--- SmartEco/Controllers/AnalyticsController.cs
+++ SmartEco/Controllers/AnalyticsController.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -15,10 +18,12 @@ namespace SmartEco.Controllers
     public class AnalyticsController : Controller
     {
         private readonly HttpApiClientController _HttpApiClient;
+        private readonly IHostingEnvironment _appEnvironment;
 
-        public AnalyticsController(HttpApiClientController HttpApiClient)
+        public AnalyticsController(HttpApiClientController HttpApiClient, IHostingEnvironment appEnvironment)
         {
             _HttpApiClient = HttpApiClient;
+            _appEnvironment = appEnvironment;
         }
 
         public IActionResult Index()
@@ -448,6 +453,226 @@ namespace SmartEco.Controllers
             postDatas = await responsePostDatass.Content.ReadAsAsync<List<PostData>>();
 
             return View(postDatas);
+        }
+
+        public IActionResult CalcAirPollutantModel()
+        {
+            List<SelectListItem> crossroads = new List<SelectListItem> {
+                new SelectListItem() { Text="MAIN ST. AND LOCAL ST. INTERSECTION", Value="1"}
+            };
+            ViewBag.Crossroads = crossroads;
+            ViewBag.DateFrom = (DateTime.Now).ToString("yyyy-MM-dd");
+            ViewBag.TimeFrom = (DateTime.Today).ToString("HH:mm:ss");
+            ViewBag.DateTo = (DateTime.Now).ToString("yyyy-MM-dd");
+            ViewBag.TimeTo = new DateTime(2000, 1, 1, 23, 59, 00).ToString("HH:mm:ss");
+
+            return View();
+        }
+
+        public class Camera
+        {
+            public int Id;
+            public string Name;
+            public decimal CoordX;
+            public decimal CoordY;
+            public decimal CoordZ;
+        }
+
+        public class Link
+        {
+            public int Id;
+            public string Name;
+            public int IQ;
+            public string Type;
+            public decimal CoordX1;
+            public decimal CoordY1;
+            public decimal CoordX2;
+            public decimal CoordY2;
+            public decimal HL;
+            public decimal WL;
+            public decimal? VPHL;
+            public decimal? EFL;
+            public int? NLANES;
+        }
+
+        public class LinkQueue
+        {
+            public int Id;
+            public int LinkId;
+            public int CAVG;
+            public int RAVG;
+            public decimal YFAC;
+            public int IV;
+            public decimal IDLFAC;
+            public int SFR;
+            public int ST;
+            public int AT;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CalcAirPollutantModel(
+            int CrossroadId,
+            DateTime DateFrom,
+            DateTime DateTo,
+            DateTime TimeFrom,
+            DateTime TimeTo)
+        {
+            string fileDat = Path.Combine(_appEnvironment.WebRootPath, "file.dat");
+            string fileBat = Path.Combine(_appEnvironment.WebRootPath, "file.bat");
+            string fileOut = Path.Combine(_appEnvironment.WebRootPath, "file.out");
+            using (var sw = new StreamWriter(fileDat))
+            {
+                var atim = 60; //переменная, представляющая среднее время(мин)
+                var zo = 175; //переменная, представляющая шероховатость поверхности(см)
+                var vs = 0; //переменная, представляющая скорость оседания(см/с)
+                var vd = 0; //реальная скорость осаждения(см/с)
+                var nr = 8; //  целое число, представляющее количество рецепторов (камер)
+                var scal = 0.3048; // коэффициент пересчета шкалы (0.3048 - футы, 1.0 - метры)
+                var iopt = 1; // целое число, метрическое преобразование вывода (1 - футы, 0 - метры)
+                var idebug = 1; // целое число, опция отладки
+
+                sw.WriteLine($"'MODEL AIR POLLUTANT CONCENTRATIONS' {atim} {zo} {vs} {vd} {nr} {scal.ToString(CultureInfo.GetCultureInfo("en-US"))} {iopt} {idebug}");
+
+                List<Camera> cameras = new List<Camera>
+                {
+                    new Camera{Id = 1, Name = "REC 1 (SE CORNER)", CoordX = 45, CoordY = -35, CoordZ = 6},
+                    new Camera{Id = 2, Name = "REC 2 (SW CORNER)", CoordX = -45, CoordY = -35, CoordZ = 6},
+                    new Camera{Id = 3, Name = "REC 3 (NW CORNER)", CoordX = -45, CoordY = 35, CoordZ = 6},
+                    new Camera{Id = 4, Name = "REC 4 (NE CORNER)", CoordX = 45, CoordY = 35, CoordZ = 6},
+                    new Camera{Id = 5, Name = "REC 5 (E MID-MAIN)", CoordX = 45, CoordY = -150, CoordZ = 6},
+                    new Camera{Id = 6, Name = "REC 6 (W MID-MAIN)", CoordX = -45, CoordY = -150, CoordZ = 6},
+                    new Camera{Id = 7, Name = "REC 7 (N MID-LOCAL)", CoordX = -150, CoordY = 35, CoordZ = 6},
+                    new Camera{Id = 8, Name = "REC 8 (S MID-LOCAL)", CoordX = -150, CoordY = -35, CoordZ = 6}
+                };
+
+                foreach (var camera in cameras)
+                {
+                    sw.WriteLine($"'{camera.Name}' {camera.CoordX.ToString(CultureInfo.GetCultureInfo("en-US"))} {camera.CoordY.ToString(CultureInfo.GetCultureInfo("en-US"))} {camera.CoordZ.ToString(CultureInfo.GetCultureInfo("en-US"))}");
+                }
+
+                var run = "MAIN ST. AND LOCAL ST.INTERSECTION"; //переменная, обозначающая название текущего цикла (перекрёстка)
+                var nl = 9; //целое число, указывающее количество ссылок
+                var nm = 1; //целое число, обозначающее количество метровых условий
+                var print2 = 0; //целое число, 0 - короткий формат, 1 - таблицы матрицы рецептор-ссылка
+                var mode = "C"; //символьная переменная( «C» для CO или «P» для твердых частиц)
+
+                sw.WriteLine($"'{run}' {nl} {nm} {print2} '{mode}'");
+
+                List<Link> links = new List<Link>
+                {
+                    new Link{Id = 1, Name = "Main St.NB Appr.", IQ = 1, Type = "AG", CoordX1 = 10, CoordY1 = -1000, CoordX2 = 10, CoordY2 = 0, VPHL = 1500, EFL = 41.6m, HL = 0, WL = 40},
+                    new Link{Id = 2, Name = "Main St.NB Queue", IQ = 2, Type = "AG", CoordX1 = 10, CoordY1 = -10, CoordX2 = 10, CoordY2 = -1000, HL = 0, WL = 20, NLANES = 2 },
+                    new Link{Id = 3, Name = "Main St.NB Dep.", IQ = 1, Type = "AG", CoordX1 = 10, CoordY1 = 0, CoordX2 = 10, CoordY2 = 1000, VPHL = 1500, EFL = 41.6m, HL = 0, WL = 40},
+                    new Link{Id = 4, Name = "Main St.SB Appr.", IQ = 1, Type = "AG", CoordX1 = -10, CoordY1 = 1000, CoordX2 = -10, CoordY2 = 0, VPHL = 1200, EFL = 41.6m, HL = 0, WL = 40},
+                    new Link{Id = 5, Name = "Main St.SB Queue", IQ = 2, Type = "AG", CoordX1 = -10, CoordY1 = 10, CoordX2 = -10, CoordY2 = 1000, HL = 0, WL = 20, NLANES = 2 },
+                    new Link{Id = 6, Name = "Main St.SB Dep.", IQ = 1, Type = "AG", CoordX1 = -10, CoordY1 = 0, CoordX2 = -10, CoordY2 = -1000, VPHL = 1200, EFL = 41.6m, HL = 0, WL = 40},
+                    new Link{Id = 7, Name = "Local St.Appr.Lnk.", IQ = 1, Type = "AG", CoordX1 = -1000, CoordY1 = 0, CoordX2 = 0, CoordY2 = 0, VPHL = 1000, EFL = 41.6m, HL = 0, WL = 40},
+                    new Link{Id = 8, Name = "Local St.Queue Lnk.", IQ = 2, Type = "AG", CoordX1 = -20, CoordY1 = 0, CoordX2 = -1000, CoordY2 = 0, HL = 0, WL = 20, NLANES = 2 },
+                    new Link{Id = 9, Name = "Main St.NB Appr.", IQ = 1, Type = "AG", CoordX1 = 0, CoordY1 = 0, CoordX2 = 1000, CoordY2 = 0, VPHL = 1000, EFL = 41.6m, HL = 0, WL = 40}
+                };
+
+                List<LinkQueue> linkQueues = new List<LinkQueue>
+                {
+                    new LinkQueue{Id = 1, LinkId = 2, CAVG = 90, RAVG = 40, YFAC = 3.0m, IV = 1500, IDLFAC = 735.00m, SFR = 0, ST = 0, AT = 0 },
+                    new LinkQueue{Id = 2, LinkId = 5, CAVG = 90, RAVG = 40, YFAC = 3.0m, IV = 1200, IDLFAC = 735.00m, SFR = 0, ST = 0, AT = 0 },
+                    new LinkQueue{Id = 3, LinkId = 8, CAVG = 90, RAVG = 50, YFAC = 3.0m, IV = 1000, IDLFAC = 735.00m, SFR = 0, ST = 0, AT = 0 }
+                };
+
+                foreach (var link in links)
+                {
+                    sw.WriteLine($"{link.IQ}");
+                    if (link.IQ == 1)
+                    {
+                        sw.WriteLine($"'{link.Name}' '{link.Type}' {link.CoordX1.ToString(CultureInfo.GetCultureInfo("en-US"))} {link.CoordY1.ToString(CultureInfo.GetCultureInfo("en-US"))} {link.CoordX2.ToString(CultureInfo.GetCultureInfo("en-US"))} {link.CoordY2.ToString(CultureInfo.GetCultureInfo("en-US"))} {link.VPHL.Value.ToString(CultureInfo.GetCultureInfo("en-US"))} {link.EFL.Value.ToString(CultureInfo.GetCultureInfo("en-US"))} {link.HL.ToString(CultureInfo.GetCultureInfo("en-US"))} {link.WL.ToString(CultureInfo.GetCultureInfo("en-US"))}");
+                    }
+                    else
+                    {
+                        sw.WriteLine($"'{link.Name}' '{link.Type}' {link.CoordX1.ToString(CultureInfo.GetCultureInfo("en-US"))} {link.CoordY1.ToString(CultureInfo.GetCultureInfo("en-US"))} {link.CoordX2.ToString(CultureInfo.GetCultureInfo("en-US"))} {link.CoordY2.ToString(CultureInfo.GetCultureInfo("en-US"))} {link.HL.ToString(CultureInfo.GetCultureInfo("en-US"))} {link.WL.ToString(CultureInfo.GetCultureInfo("en-US"))} {link.NLANES}");
+                        var linkQueue = linkQueues.Where(l => l.LinkId == link.Id).FirstOrDefault();
+                        if (linkQueue != null)
+                        {
+                            sw.WriteLine($"{linkQueue.CAVG} {linkQueue.RAVG} {linkQueue.YFAC.ToString(CultureInfo.GetCultureInfo("en-US"))} {linkQueue.IV} {linkQueue.IDLFAC.ToString(CultureInfo.GetCultureInfo("en-US"))} {linkQueue.SFR} {linkQueue.ST} {linkQueue.AT}");
+                        }
+                    }
+                }
+
+                var u = 1.0; //действительная переменная, указывающая скорость ветра(м/с)
+                var brg = 0; // действительная переменная, указывающая направление ветра (угол)
+                var clas = 4; //целое число, обозначающее класс стабильности
+                var mixh = 1000; //действительная переменная, представляющая высоту смешивания(м)
+                var amb = 0; // действительное значение, указывающее фоновую концентрацию окружающей среды(ppm)
+                var variable = "Y"; // «Y» - изменение направления ветра, «N» - только одно направление ветра
+                var degr = 10; // целое число, метрическое преобразование вывода (1 - футы, 0 - метры)
+                var vai1 = 0; // целое число, обозначающее нижнюю границу диапазона вариации
+                var vai2 = 36; // целое число, которое указывает верхнюю границу диапазона изменения
+
+                sw.WriteLine($"{u} {brg} {clas} {mixh} {amb} '{variable}' {degr} {vai1} {vai2}");
+            }
+
+            using (var sw = new StreamWriter(fileBat))
+            {
+                sw.WriteLine($"CAL3QHC file.dat file.out");
+            }
+
+            Process process = new Process();
+            try
+            {
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+
+
+                process.StartInfo.WorkingDirectory = _appEnvironment.WebRootPath;
+                process.StartInfo.FileName = fileBat;
+                process.Start();
+
+                string output = "",
+                    error = "";
+                while (!process.StandardOutput.EndOfStream)
+                {
+                    output += process.StandardOutput.ReadLine();
+                }
+                while (!process.StandardError.EndOfStream)
+                {
+                    error += process.StandardError.ReadLine();
+                }
+
+                process.WaitForExit();
+                System.IO.File.Delete(fileBat);
+                System.IO.File.Delete(fileDat);
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exception.ToString(), exception.InnerException);
+            }
+
+            List<string> matrix = new List<string>();
+
+            using (StreamReader sr = new StreamReader(fileOut, System.Text.Encoding.Default))
+            {
+                string line;
+                while (!sr.ReadLine().Contains("(PPM)"))
+                {
+                }
+                do
+                {
+                    line = sr.ReadLine();
+                    matrix.Add(line);
+                }
+                while (line != null);
+                ViewBag.Matrix = matrix;
+            }
+
+            List<SelectListItem> crossroads = new List<SelectListItem> {
+                new SelectListItem() { Text="MAIN ST. AND LOCAL ST. INTERSECTION", Value="1"}
+            };
+            ViewBag.Crossroads = crossroads;
+            ViewBag.DateFrom = (DateTime.Now).ToString("yyyy-MM-dd");
+            ViewBag.TimeFrom = (DateTime.Today).ToString("HH:mm:ss");
+            ViewBag.DateTo = (DateTime.Now).ToString("yyyy-MM-dd");
+            ViewBag.TimeTo = new DateTime(2000, 1, 1, 23, 59, 00).ToString("HH:mm:ss");
+
+            return View();
         }
     }
 }
