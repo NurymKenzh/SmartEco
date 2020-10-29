105cbe54e73aadec7fa6c65b069b2272c38bde5b
Dasha Tr <dtitpr@gmail.com>
2020-10-29 13:39:32 +0200

    translations SpeciesDiversity

--- SmartEco/Resources/Controllers/SharedResources.Designer.cs
+++ SmartEco/Resources/Controllers/SharedResources.Designer.cs
@@ -19,7 +19,7 @@ namespace SmartEco.Resources.Controllers {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class SharedResources {
@@ -2127,6 +2127,24 @@ namespace SmartEco.Resources.Controllers {
         public static string SpeciallyProtectedNaturalTerritory {
             get {
                 return ResourceManager.GetString("SpeciallyProtectedNaturalTerritory", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to .
+        /// </summary>
+        public static string SpeciesDiversities {
+            get {
+                return ResourceManager.GetString("SpeciesDiversities", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to .
+        /// </summary>
+        public static string SpeciesDiversity {
+            get {
+                return ResourceManager.GetString("SpeciesDiversity", resourceCulture);
             }
         }
         
