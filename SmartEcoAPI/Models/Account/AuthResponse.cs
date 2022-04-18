24c56587f9bc38aec4622b53ff64c4640034e120
Avtr-Elias <ilyaitpr@gmail.com>
2022-04-18 16:48:05 +0300

    SmartEcoAPI, SmartEco: change JSON register/auth account

---
+++ SmartEcoAPI/Models/Account/AuthResponse.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartEcoAPI.Models.Account
+{
+    public class AuthResponse
+    {
+        [JsonProperty("accessToken")]
+        public string AccessToken { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("role")]
+        public string Role { get; set; }
+
+        [JsonProperty("message")]
+        public string Message { get; set; }
+    }
+}
