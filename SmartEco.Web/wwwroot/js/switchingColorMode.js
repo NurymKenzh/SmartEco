0dfc918d42882cc6ca1298bb2024f052b7be9446
Avtr-Elias <ilyaitpr@gmail.com>
2023-04-10 12:59:12 +0300

    SmartEco.Web: remove flicker of display in dark mode

--- SmartEco.Web/wwwroot/js/switchingColorMode.js
+++ SmartEco.Web/wwwroot/js/switchingColorMode.js
@@ -17,26 +17,20 @@ function switchTheme(e) {
     }
 }
 
-//determines if the user has a set theme
+//switch position initialization
 function detectColorScheme() {
     var theme = "light";    //default to light
 
     //local storage is used to override OS theme settings
     if (localStorage.getItem("theme")) {
         if (localStorage.getItem("theme") == "dark") {
-            var theme = "dark";
+            toggleSwitch.checked = true;
         }
     } else if (!window.matchMedia) {
         //matchMedia method not supported
         return false;
     } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
         //OS theme setting detected as dark
-        var theme = "dark";
-    }
-
-    //dark theme preferred, set document with a `data-theme` attribute
-    if (theme == "dark") {
-        document.documentElement.setAttribute("data-bs-theme", "dark");
         toggleSwitch.checked = true;
     }
 }
